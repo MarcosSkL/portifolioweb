9327a4c707d56822fb6e59841f357c2ee5b0e66c
Marcosskl <marcos.marsk@gmail.com>
2023-09-15 21:16:32 -0300

    Pequena alteração na grid-col da div

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,17 +6,17 @@ const About = () => {
       <div className='w-full h-screen bg-slate-700 text-gray-300'>
         <div className='flex flex-col justify-center items-center w-full h-full'>
           <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
-            <div className='text-4xl font-bold inline border-b-4 border-cyan-400'>
-              <p>About</p>
+            <div className='sm:text-right pb-8 pl-4'>
+              <p className='text-4xl font-bold inline border-b-4 border-cyan-400'>About</p>
             </div>
             <div>
-
+              
             </div>
           </div>
 
         </div>
 
-      </div>
+      </div >
     </>
   )
 }
