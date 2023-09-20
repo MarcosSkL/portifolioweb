9f9ed9b3a396675df07c976283c3cdbbcbc7af22
Marcosskl <marcos.marsk@gmail.com>
2023-09-19 23:50:48 -0300

    Add link nos <a> da Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -48,21 +48,21 @@ const Navbar = () => {
             <li className='w-[160px] h-[60px] flex justify-between item-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
               <a
                 className='flex justify-between items-center w-full text-gray-300 '
-                href=''>
+                href='https://www.linkedin.com/in/marcos-bezerra-skl/' target='_blank'>
                 Linkedin <FaLinkedin size={38} />
               </a>
             </li>
             <li className='w-[160px] h-[60px] flex justify-between item-center ml-[-100px] hover:ml-[-10px] duration-300 bg-gray-800'>
               <a
                 className='flex justify-between items-center w-full text-gray-300 '
-                href=''>
+                href='https://github.com/MarcosSkL' target='_blank'>
                 Github <FaGithub size={38} />
               </a>
             </li>
             <li className='w-[160px] h-[60px] flex justify-between item-center ml-[-100px] hover:ml-[-10px] duration-300 bg-emerald-500'>
               <a
                 className='flex justify-between items-center w-full text-gray-300 '
-                href=''>
+                href='marcos.marsk@gmail.com' target='_blank'>
                 Email <HiOutlineMail size={38} />
               </a>
             </li>
