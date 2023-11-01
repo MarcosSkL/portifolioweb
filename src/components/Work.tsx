5d82c94d3a5d5eaf7004395532ac74c84991922f
Marcos <marcos.marsk@gmail.com>
2023-10-31 23:45:25 -0300

    Add Projeto saiba politico

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -15,7 +15,7 @@ const Work = () => {
                      {/* Hover Effects */}
                      <div className='text-center items-center justify-center flex flex-col group-hover:bg-cyan-600 group-hover:bg-opacity-90 hover:rounded-lg opacity-0 group-hover:opacity-100 w-full'>
                         <span className='text-2xl font-bold text-white tracking-wide'>
-                           React TS Application
+                           Next TS WEB Application
                         </span>
                         <div className='pt-8 text-center'>
                            <a href='https://gamestoreskl.vercel.app/' target='_blanck'>
@@ -29,12 +29,31 @@ const Work = () => {
 
                      </div>
                   </div>
+                  <div className="bg-[url('../../public/assets/Projetos/Saiba_Politico.png')] bg-cover bg-center bg-no-repeat shadow-lg shadow-slate-700 group container rounded-md flex justify-center item-center mx-auto h-[250px]">
+
+                     {/* Hover Effects */}
+                     <div className='text-center items-center justify-center flex flex-col group-hover:bg-cyan-600 group-hover:bg-opacity-90 hover:rounded-lg opacity-0 group-hover:opacity-100 w-full'>
+                        <span className='text-2xl font-bold text-white tracking-wide'>
+                           Next JS WEB
+                        </span>
+                        <div className='pt-8 text-center'>
+                           <a href='https://saiba-politico.vercel.app/' target='_blanck'>
+                              <button className='text-center rounded-lg px-4 py-3 m-2 bg-white hover:bg-sky-400 text-gray-700 font-bold text-lg'>Demo</button>
+                           </a>
+                           <a href='https://github.com/MarcosSkL/Saiba_Politico' target='_blanck'>
+                              <button className='text-center rounded-lg px-4 py-3 m-2 bg-white hover:bg-sky-400 text-gray-700 font-bold text-lg'>Code</button>
+                           </a>
+
+                        </div>
+
+                     </div>
+                  </div>
                   <div className="bg-[url('../../public/assets/Projetos/Parallax.png')] bg-cover bg-center bg-no-repeat shadow-lg shadow-slate-700 group container rounded-md flex justify-center item-center mx-auto h-[250px]">
 
                      {/* Hover Effects */}
                      <div className='text-center items-center justify-center flex flex-col group-hover:bg-cyan-600 group-hover:bg-opacity-90 hover:rounded-lg opacity-0 group-hover:opacity-100 w-full'>
                         <span className='text-2xl font-bold text-white tracking-wide'>
-                           React TS Application
+                           HTML5 CSS3
                         </span>
                         <div className='pt-8 text-center'>
                            <a href='https://marcosskl.github.io/Parallax_HTML5-e-CSS3/' target='_blanck'>
@@ -53,7 +72,7 @@ const Work = () => {
                      {/* Hover Effects */}
                      <div className='text-center items-center justify-center flex flex-col group-hover:bg-cyan-600 group-hover:bg-opacity-90 hover:rounded-lg opacity-0 group-hover:opacity-100 w-full'>
                         <span className='text-2xl font-bold text-white tracking-wide'>
-                           React TS Application
+                           HTML5 CSS3
                         </span>
                         <div className='pt-8 text-center'>
                            <a href='https://marcosskl.github.io/Think-of-Number/' target='_blanck'>
