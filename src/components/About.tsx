630634d91def19cfb3a1b4750b181268474b35c1
Marcos <marcos.marsk@gmail.com>
2023-11-09 23:54:22 -0300

    Criado um novo componente com a função de animação entre cada seção do site

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,32 +1,44 @@
 import React from 'react';
+import { useRef } from 'react';
+import useOnScreen from './UseScreen';
 
 const About = () => {
+
+  const ref = useRef();
+  const onScreen = useOnScreen(ref, '-300px');
+
   return (
     <>
+
       <div id='about' className='w-full h-screen bg-slate-700 text-gray-300'>
         <div className='flex flex-col justify-center items-center w-full h-full'>
-          <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
-            <div className='sm:text-right pb-8 pl-4'>
-              <p
-                className='text-4xl font-bold inline border-b-4 border-cyan-400'>
-                Sobre
-              </p>
-            </div>
-            <div>
+          <div
+            ref={ref}
+            className={`animate transform transition-all duration-1000 ease-out ${onScreen ? 'translate-y-0 opacity-100' : '-translate-y-52 opacity-0'}`}
+          >
+            <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+              <div className='sm:text-right pb-8 pl-4'>
+                <p
+                  className='text-4xl font-bold inline border-b-4 border-cyan-400'>
+                  Sobre
+                </p>
+              </div>
+              <div>
 
+              </div>
             </div>
-          </div>
-          <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
-            <div className='sm:text-right text-4xl font-bold'>
-              <p>A História por Trás do Código<span className='text-cyan-400'>...</span><span className='animate-blink text-cyan-400'>|</span></p>
-            </div>
-            <div>
-              <p>"Sou Desenvolvedor WEB e Mobile, e de olho no Backend. Amante da tecnologia desde pequeno,
-                estou sempre em busca de novos desafios e aprendizagem na área. Ex-militar da Força Aérea
-                Brasileira e Músico, estou migrando para a área da tecnologia, com bastante carga de conhecimento,
-                na qual adquirir em diversas áreas que atuei. Meu objetivo é criar, manter e atualizar,
-                interfaces e aplicações funcionais e modernas, que atendam às necessidades dos usuários, empresas e clientes.
-                Entre em contato comigo, vamos conversar sobre como eu posso contribuir para a sua empresa ou projeto".</p>
+            <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+              <div className='sm:text-right text-4xl font-bold'>
+                <p>A História por Trás do Código<span className='text-cyan-400'>...</span><span className='animate-blink text-cyan-400'>|</span></p>
+              </div>
+              <div>
+                <p>"Sou Desenvolvedor WEB e Mobile, e de olho no Backend. Amante da tecnologia desde pequeno,
+                  estou sempre em busca de novos desafios e aprendizagem na área. Ex-militar da Força Aérea
+                  Brasileira e Músico, estou migrando para a área da tecnologia, com bastante carga de conhecimento,
+                  na qual adquirir em diversas áreas que atuei. Meu objetivo é criar, manter e atualizar,
+                  interfaces e aplicações funcionais e modernas, que atendam às necessidades dos usuários, empresas e clientes.
+                  Entre em contato comigo, vamos conversar sobre como eu posso contribuir para a sua empresa ou projeto".</p>
+              </div>
             </div>
           </div>
         </div>
---
+++ src/components/UseScreen.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState, RefObject } from 'react';
+
+function useOnScreen(ref: RefObject<HTMLElement>): boolean {
+    const [isIntersecting, setIntersecting] = useState(false);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                setIntersecting(entry.isIntersecting);
+            },
+           
+        );
+        if (ref.current) {
+            observer.observe(ref.current);
+        }
+        return () => {
+            if (ref.current) {
+                observer.unobserve(ref.current);
+            }
+        };
+        }, []);
+
+    return isIntersecting;
+}
+
+export default useOnScreen;
