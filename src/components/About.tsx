0fea60b26419ae09f9bc97008771c5a879c43436
Marcos F. Bezerra <marcos.marsk@gmail.com>
2024-01-09 23:44:41 -0300

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -32,7 +32,7 @@ const About = () => {
                 <p>A História por Trás do Código<span className='text-cyan-400'>...</span><span className='animate-blink text-cyan-400'>|</span></p>
               </div>
               <div>
-                <p>Sou Desenvolvedor WEB e Mobile, e de olho no Backend. Amante da tecnologia desde pequeno,
+                <p>Sou Analista e Desenvolvedor WEB, Mobile e de olho no Backend. Amante da tecnologia desde pequeno,
                   estou sempre em busca de novos desafios e aprendizagem na área. Ex-militar da Força Aérea
                   Brasileira e Músico, estou migrando para a área da tecnologia, com bastante carga de conhecimento,
                   na qual adquirir em diversas áreas que atuei. Meu objetivo é criar, manter e atualizar,
